6aa5ad23cebf60490e3c7ac3bd5da90a87e3aba2
ArvinCS <arvinciu86@yahoo.com>
2023-09-20 12:04:04 +0700

    feat: add django test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+from main.models import Item
+
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/')
+        self.assertTemplateUsed(response, 'main.html')
+    
+    def test_assign_item(self):
+        # Create item
+        Item.objects.create(
+            name = 'Arabica',
+            amount = 1,
+            description = 'Most common coffee beans in Indonesia',
+            taste = 'Aromatic but bittersweet'
+        )
+
+        # Validate created item
+        arabica = Item.objects.get(name = 'Arabica')
+        self.assertEqual(arabica.amount, 1)
+        self.assertEqual(arabica.description, 'Most common coffee beans in Indonesia')
+        self.assertEqual(arabica.taste, 'Aromatic but bittersweet')
