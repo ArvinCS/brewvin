1ee571430ac9e7b8b022f5b3c2b18e397df4eefe
ArvinCS <arvinciu86@yahoo.com>
2023-11-15 20:42:24 +0700

    feat: add Whitenoise middleware and CompressedManifestStaticFilesStorage for serving static files.

--- brewvin/settings.py
+++ brewvin/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'brewvin.urls'
@@ -135,6 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
