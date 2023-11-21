a5592a163fa1176ea0236e4f84a5c20816deb932
ArvinCS <arvinciu86@yahoo.com>
2023-11-22 00:41:36 +0700

    fix: allowed host and auth

--- brewvin/settings.py
+++ brewvin/settings.py
@@ -154,7 +154,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
 
-ALLOWED_HOSTS = ["arvin-tugas.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["arvin-tugas.pbp.cs.ui.ac.id", "127.0.0.1", "localhost"]
 CSRF_TRUSTED_ORIGINS = ["https://arvin-tugas.pbp.cs.ui.ac.id"]
 CSRF_ALLOWED_ORIGINS = ["https://arvin-tugas.pbp.cs.ui.ac.id"]
 CORS_ORIGINS_WHITELIST = ["https://arvin-tugas.pbp.cs.ui.ac.id"]
